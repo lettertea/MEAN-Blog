7221b0e4cb982c68011c7fd39b22cc08364e745d
Thomas Le <le.thomas.vu@gmail.com>
2018-01-01 22:08:06 -0800

    deployment

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto').randomBytes(256).toString('hex');
 
 module.exports = {
-    uri: process.env.db,
+    uri: 'mongodb://deadpools:Idontknow123@ds135747.mlab.com:35747/mea2n-blog',
     secret: crypto,
     db: process.env.databaseName
 }
