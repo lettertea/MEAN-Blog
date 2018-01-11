b4641d8bec07923dd2ef0087c774953fd39b30c6
Thomas Le <le.thomas.vu@gmail.com>
2018-01-11 10:10:20 -0800

    changed edit and delete buttons

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto').randomBytes(256).toString('hex');
 
 module.exports = {
-    uri: 'mongodb://deadpools:Idontknow123@ds135747.mlab.com:35747/mea2n-blog',
+    uri: process.env.databaseUri,
     secret: crypto,
     db: process.env.databaseName
 }
